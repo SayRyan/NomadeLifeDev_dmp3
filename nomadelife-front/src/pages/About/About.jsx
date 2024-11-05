6f99f7bea3d824d8ff78bc7da1dcc8edf30c92f2
SayRyan <richardryancarvalho.infotarde@gmail.com>
2024-11-05 12:41:24 -0300

    Aula1105 - Componentização SPA

---
+++ nomadelife-front/src/pages/About/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import logo from '../../../public/logo-quadrada.png';
+
+export const About = () => {
+  return (
+    <div>
+        <img src={logo} alt="Logo Desenvolvedora" />
+    </div>
+  )
+}
